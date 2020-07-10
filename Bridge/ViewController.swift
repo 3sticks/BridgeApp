be41f2956bd44a9cfed2404d7e886d1c2eb248dc
Hugo Bucci <hugorbucci@gmail.com>
2020-07-10 18:16:35 -0400

    Initial Commit

---
+++ Bridge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bridge
+//
+//  Created by Hugo Bucci III on 7/10/20.
+//  Copyright © 2020 Hugo Bucci III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
